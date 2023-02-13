89dbcde63ac49c9fa98ef18d76c3feff1e3193d0
Stalin <sta_vla411@hotmail.com>
2023-02-13 18:51:24 -0500

    change context

--- src/components/context/languageContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from 'react'
-
-const langContext = createContext()
-
-function LanguageContext({ children }) {
-  return <langContext.Provider>{children}</langContext.Provider>
-}
-
-export { LanguageContext, langContext }
