ec22a8c84058f9074397c2579a5efc992da9cb07
Stalin <sta_vla411@hotmail.com>
2023-06-05 21:32:53 -0500

    change name and light theme

--- src/style/Theme.jsx
+++ src/style/Theme.jsx
@@ -1,8 +1,16 @@
-export const DarkTheme = {
+export const dark = {
   Title: 'rgb(255, 193, 5)',
   Subtitle: '#FFFFFF',
   Description: '#ABB2BF',
   Background: '#282C33',
+  WidthSection: '65rem',
+  Hover: 'hsl(45deg 100% 51% / 20%)'
+}
+export const light = {
+  Title: 'rgb(250, 204, 21)',
+  Subtitle: 'rgb(34, 42, 53)',
+  Description: '#ABB2BF',
+  Background: 'rgb(232, 234, 236)',
   WidthSection: '65rem',
   Hover: 'hsl(45deg 100% 51% / 20%)'
 }
